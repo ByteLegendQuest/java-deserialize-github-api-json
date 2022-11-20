ea52efa7ffcf84b8f5f96d3f9f29af121fce8478
keranbingaa <binghong@mail.ustc.edu.cn>
2022-11-20 09:13:52 +0000

    Update at 2022-11-20T09:13:52.678372Z

--- src/main/java/com/bytelegend/GitHubApiCommitObject.java
+++ src/main/java/com/bytelegend/GitHubApiCommitObject.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 public class GitHubApiCommitObject {
     private String sha;
