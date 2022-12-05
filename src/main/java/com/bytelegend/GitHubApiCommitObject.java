2470975bd970f1c710a0913433da7b5f5928eba2
johnsonxiaoyu <1272304153@qq.com>
2022-12-05 08:40:30 +0000

    Update at 2022-12-05T08:40:30.787555Z

--- src/main/java/com/bytelegend/GitHubApiCommitObject.java
+++ src/main/java/com/bytelegend/GitHubApiCommitObject.java
@@ -1,5 +1,6 @@
 package com.bytelegend;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,8 +8,255 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 public class GitHubApiCommitObject {
+    private Author author;
+    private Committer committer;
+    private List<Parents> parents;
+    private Verification verification;
+    private Tree tree;
+
+    private String sha;
+    @JsonProperty("node_id")
+    private String nodeId;
+    private String url;
+    @JsonProperty("html_url")
+    private String htmlUrl;
+    private String message;
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Tree getTree() {
+        return tree;
+    }
+
+    public void setTree(Tree tree) {
+        this.tree = tree;
+    }
+
+    public String getSha() {
+        return sha;
+    }
+
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Committer getCommitter() {
+        return committer;
+    }
+
+    public void setCommitter(Committer committer) {
+        this.committer = committer;
+    }
+
+    public List<Parents> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Parents> parents) {
+        this.parents = parents;
+    }
+
+    public Verification getVerification() {
+        return verification;
+    }
+
+    public void setVerification(Verification verification) {
+        this.verification = verification;
+    }
+
+    public class Author {
+
+        private String Date;
+        private String name;
+        private String email;
+
+        public String getDate() {
+            return Date;
+        }
+
+        public void setDate(String date) {
+            Date = date;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+    public class Committer {
+
+        private String Date;
+        private String name;
+        private String email;
+
+        public String getDate() {
+            return Date;
+        }
+
+        public void setDate(String date) {
+            Date = date;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+    public class Parents {
+        private String sha;
+        private String url;
+        @JsonProperty("html_url")
+        private String htmlUrl;
+
+        public String getHtmlUrl() {
+            return htmlUrl;
+        }
+
+        public void setHtmlUrl(String htmlUrl) {
+            this.htmlUrl = htmlUrl;
+        }
+
+        public String getSha() {
+            return sha;
+        }
+
+        public void setSha(String sha) {
+            this.sha = sha;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+    public class Tree {
+        private String sha;
+        private String url;
+
+        public String getSha() {
+            return sha;
+        }
+
+        public void setSha(String sha) {
+            this.sha = sha;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+    public class Verification {
+
+        private String reason;
+        private String signature;
+        private String payload;
+        private boolean verified;
+
+        public Boolean isVerified(){
+            return verified;
+        }
+
+
+        public String getReason() {
+            return reason;
+        }
+
+        public void setReason(String reason) {
+            this.reason = reason;
+        }
+
+        public String getSignature() {
+            return signature;
+        }
+
+        public void setSignature(String signature) {
+            this.signature = signature;
+        }
+
+        public String getPayload() {
+            return payload;
+        }
+
+        public void setPayload(String payload) {
+            this.payload = payload;
+        }
+    }
+    
     public static GitHubApiCommitObject fromJson(String json) throws JsonProcessingException {
         ObjectMapper mapper =
                 new ObjectMapper()
