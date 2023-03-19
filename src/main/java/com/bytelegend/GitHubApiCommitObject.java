21a511384cad0a3d27da486e885ecfc2faaadc36
guakun01 <guakun01@gmail.com>
2023-03-19 05:33:29 +0000

    Update at 2023-03-19T05:33:29.802133Z

--- src/main/java/com/bytelegend/GitHubApiCommitObject.java
+++ src/main/java/com/bytelegend/GitHubApiCommitObject.java
@@ -9,6 +9,211 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class GitHubApiCommitObject {
+    private String sha;
+
+    private String nodeId;
+
+    private String url;
+
+    private String htmlUrl;
+
+    private User author;
+
+    private User committer;
+
+    private String message;
+
+    private Item tree;
+
+    private List<Item> parents;
+
+    private Verfication verfication;
+
+    public String getSha() {
+        return sha;
+    }
+
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public User getCommitter() {
+        return committer;
+    }
+
+    public void setCommitter(User committer) {
+        this.committer = committer;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Item getTree() {
+        return tree;
+    }
+
+    public void setTree(Item tree) {
+        this.tree = tree;
+    }
+
+    public List<Item> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Item> parents) {
+        this.parents = parents;
+    }
+
+    public Verfication getVerfication() {
+        return verfication;
+    }
+
+    public void setVerfication(Verfication verfication) {
+        this.verfication = verfication;
+    }
+    
+    private class User {
+        private Date date;
+        private String name;
+
+        private String email;
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+    private class Item {
+
+        private String url;
+
+        private String sha;
+
+        private String htmlUrl;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getSha() {
+            return sha;
+        }
+
+        public void setSha(String sha) {
+            this.sha = sha;
+        }
+
+        public String getHtmlUrl() {
+            return htmlUrl;
+        }
+
+        public void setHtmlUrl(String htmlUrl) {
+            this.htmlUrl = htmlUrl;
+        }
+    }
+
+    private class Verfication {
+        private Boolean verified;
+
+        private String reason;
+
+        private String signature;
+
+        private String payload;
+
+        public Boolean getVerified() {
+            return verified;
+        }
+
+        public void setVerified(Boolean verified) {
+            this.verified = verified;
+        }
+
+        public String getReason() {
+            return reason;
+        }
+
+        public void setReason(String reason) {
+            this.reason = reason;
+        }
+
+        public String getSignature() {
+            return signature;
+        }
+
+        public void setSignature(String signature) {
+            this.signature = signature;
+        }
+
+        public String getPayload() {
+            return payload;
+        }
+
+        public void setPayload(String payload) {
+            this.payload = payload;
+        }
+    }
     public static GitHubApiCommitObject fromJson(String json) throws JsonProcessingException {
         ObjectMapper mapper =
                 new ObjectMapper()
