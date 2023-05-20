c848c175a7f4282a913c2aa171b40bf9cd8754fc
DasBrain <johannes13@johannes13.de>
2023-05-20 19:19:15 +0000

    Update at 2023-05-20T19:19:15.755682Z

--- src/main/java/com/bytelegend/GitHubApiCommitObject.java
+++ src/main/java/com/bytelegend/GitHubApiCommitObject.java
@@ -20,4 +20,91 @@ public class GitHubApiCommitObject {
         String json = new String(Files.readAllBytes(new File("./commit.json").toPath()));
         GitHubApiCommitObject commit = GitHubApiCommitObject.fromJson(json);
     }
+
+    private String sha;
+    public String getSha() {
+        return sha;
+    }
+
+    private String nodeId;
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    private String url;
+    public String getUrl() {
+        return url;
+    }
+
+    private String htmlUrl;
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    private Person author;
+    public Person getAuthor() {
+        return author;
+    }
+    
+    private Person committer;
+    public Person getCommitter() {
+        return committer;
+    }
+
+    private String message;
+    public Object getMessage() {
+        return message;
+    }
+
+    private GitHubApiCommitObject tree;
+    public GitHubApiCommitObject getTree() {
+        return tree;
+    }
+
+    private List<GitHubApiCommitObject> parents = new ArrayList<>();
+    public List<GitHubApiCommitObject> getParents() {
+        return parents;
+    }
+
+    private Verification verification;
+    public Verification getVerification() {
+        return verification;
+    }
+}
+
+class Person {
+
+    private String date;
+    public String getDate() {
+        return date;
+    }
+    private String name;
+    public String getName() {
+        return name;
+    }
+    private String email;
+    public String getEmail() {
+        return email;
+    }
+}
+
+class Verification {
+
+    private boolean verified;
+    public boolean isVerified() {
+        return verified;
+    }
+    private String reason;
+    public String getReason() {
+        return reason;
+    }
+    private String signature;
+    public String getSignature() {
+        return signature;
+    }
+    private String payload;
+    public String getPayload() {
+        return payload;
+    }
+    
 }
