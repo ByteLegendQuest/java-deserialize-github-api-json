9129028221505f7ce68c7431fc5180287773c4c5
andyliukun <744059787@qq.com>
2023-06-08 02:02:37 +0000

    Update at 2023-06-08T02:02:37.895156Z

--- src/main/java/com/bytelegend/GitHubApiCommitObject.java
+++ src/main/java/com/bytelegend/GitHubApiCommitObject.java
@@ -7,8 +7,74 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
-public class GitHubApiCommitObject {
+public class GitHubApiCommitObject extends NodeWithHtml {
+
+    String node_id;
+
+    public String getNodeId() {
+        return node_id;
+    }
+
+    public void setNode_id(String node_id) {
+        this.node_id = node_id;
+    }
+    Person author;
+    Person committer;
+    String message;
+    Node tree;
+    List<NodeWithHtml> parents;
+    Verification verification;
+
+    public Person getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Person author) {
+        this.author = author;
+    }
+
+    public Person getCommitter() {
+        return committer;
+    }
+
+    public void setCommitter(Person committer) {
+        this.committer = committer;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Node getTree() {
+        return tree;
+    }
+
+    public void setTree(Node tree) {
+        this.tree = tree;
+    }
+
+    public List<NodeWithHtml> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<NodeWithHtml> parents) {
+        this.parents = parents;
+    }
+
+    public Verification getVerification() {
+        return verification;
+    }
+
+    public void setVerification(Verification verification) {
+        this.verification = verification;
+    }
+
     public static GitHubApiCommitObject fromJson(String json) throws JsonProcessingException {
         ObjectMapper mapper =
                 new ObjectMapper()
